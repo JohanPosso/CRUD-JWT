7ca259b56293036e1b8476f3203bd82d0fdd512e
JohanPosso <johan-posito2013@hotmail.com>
2023-05-10 02:08:12 +0100

    feat: add counter for table

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let containerData = [];
 
 const templateElement = document.getElementById("templateElement").content;
+// const templateEdit = document.getElementById("templateEdit").content;
 const tablebody = document.getElementById("tablebody");
 const form = document.querySelector("#form");
 const submitData = document.querySelector(".submitData");
@@ -12,7 +13,6 @@ const lastname = document.getElementById("lastname");
 const phone = document.getElementById("phone");
 const company = document.getElementById("company");
 const email = document.getElementById("email");
-const password = document.getElementById("password");
 
 tablebody.addEventListener("click", (e) => {
   eliminarData(e);
@@ -28,7 +28,6 @@ form.addEventListener("submit", (e) => {
       phone: phone.value,
       company: company.value,
       email: email.value,
-      password: password.value,
     },
   ];
 
@@ -45,12 +44,13 @@ const pintarCards = () => {
   while (tablebody.firstChild) {
     tablebody.removeChild(tablebody.firstChild);
   }
-
+  let id = 0;
   containerData.forEach((item) => {
+    id++;
+
     templateElement.querySelector("#btnEliminar").dataset.id = item.id;
     templateElement.querySelector("#btnEditar").dataset.id = item.id;
-    templateElement.querySelector("#nameElement").textContent = item.name;
-    templateElement.querySelector("#idElement").textContent = item.id;
+    templateElement.querySelector("#idElement").textContent = id;
     templateElement.querySelector("#nameElement").textContent = item.name;
     templateElement.querySelector("#lastNameElement").textContent =
       item.lastname;
@@ -79,6 +79,16 @@ const eliminarData = (e) => {
 
 const editarData = (e) => {
   if (e.target.classList.contains("btn-warning")) {
-    alert("");
+    const findElement = containerData.find(
+      (ele) => ele.id == e.target.dataset.id
+    );
+
+    if (e.target.dataset.id == findElement.id) {
+      document.getElementById("name2").value = findElement.name;
+      document.getElementById("lastname2").value = findElement.lastname;
+      document.getElementById("phone2").value = findElement.phone;
+      document.getElementById("company2").value = findElement.company;
+      document.getElementById("email2").value = findElement.email;
+    }
   }
 };
